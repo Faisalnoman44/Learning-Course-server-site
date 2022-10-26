adc43fd23875d155983e766fd5e2f9c205a35229
Faisalnoman44 <mdnoman.mn11@gmail.com>
2022-10-26 12:59:15 +0600

    catagory data send successfully

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express')
+const express = require('express');
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+
+const catagories = require('./data/catagories.json')
+
+
 app.get('/', (req, res) =>{
-    res.send('data dite pacchi')
-});
+    res.send('data jacche');
+})
+
+app.get('/catagories', (req, res) =>{
+    res.send(catagories)
+})
 
 app.listen(port, () =>{
-    console.log('server ta kaj kortese', port)
-});
+    console.log('server kaj ki kortese', port);
+})
